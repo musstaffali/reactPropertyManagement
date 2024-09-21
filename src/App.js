65167a476b85b3e5837890eb8a9a249eac9aa212
Mustafa <mamusstaffali@gmail.com>
2024-09-20 21:34:30 -0400

    landlord js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux'
+import Home from './containers/Home'
+import Navbar from './components/Navbar';
+import LandlordList from './containers/landlords/LandlordList'
+import LandlordForm from './containers/landlords/LandlordForm'
+import LandlordShow from './containers/landlords/LandlordShow'
+import { fetchLandlords } from './actions/landlords'
+import LandlordHousesList from './containers/houses/LandlordHousesList';
+import HouseForm from './containers/houses/HouseForm'
+import LandlordHouseUpdate from './containers/landlords/LandlordHouseUpdate';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+
+  state = {
+    landlords: []
+  }
+  componentDidMount() {
+    this.props.fetchLandlords()
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+
+          <div class="d-flex justify-content-center align-items-center" >
+            <Navbar />
+          </div>
+
+          <div class="d-flex justify-content-center align-items-center">
+            {/* <img src='images/img-landlord.jpg'/> */}
+
+            <Switch>
+
+              <Route exact path="/" component={Home} />
+              <Route exact path="/landlords" component={LandlordList} />
+              <Route exact path="/landlords/new" component={LandlordForm} />
+              <Route exact path="/houses/new" component={HouseForm} />
+              {/* <Route exact path="/landlords/:id" render={props => <LandlordShow {...props} landlords={this.state.landlords} />} /> */}
+              <Route exact path="/landlords/:id" component={LandlordShow} />
+              <Route exact path="/landlords/:id/houses/new" component={HouseForm} />
+              <Route exact path="/landlords/:id/houses" component={LandlordHousesList} />
+              <Route exact path="/landlords/:id/houses/:houseid/edit" component={LandlordHouseUpdate} />
+              <Route render={() => <div>This page doesnot exist!</div>} />
+            </Switch>
+          </div>
+
+
+        </div>
+
+      </Router>
+
+    );
+  }
+
 }
 
-export default App;
+export default connect(null, { fetchLandlords })(App);
